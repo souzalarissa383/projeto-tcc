0a274aba6bbccdb2485d0a85901865c613ec93a9
“larissa.ribeiro” <larissa.ribeiro@g.globo>
2023-05-27 22:51:02 -0300

    Menu

--- src/Pages/Menu/index.jsx
+++ src/Pages/Menu/index.jsx
@@ -1,10 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+//import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./style.css";
-
-
 import React from 'react'
 
 
@@ -16,17 +14,17 @@ function MenuNavbar() {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav 
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px',}}
+            className="me-auto my-2 my-lg-0 "
+            style={{ maxHeight: '100px'}}
             navbarScroll
           >
-            <Nav.Link  href="#action1">Para você</Nav.Link>
+            <Nav.Link href="#action1">Para você</Nav.Link>
             <Nav.Link href="#action2">Para empresas</Nav.Link>
            
            
           </Nav>
        
-          <Nav.Link  href="#action1">Login</Nav.Link>
+          <Nav.Link  href="#action3">Login</Nav.Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
